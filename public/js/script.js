5fd00ae3ffa81a0ec6f91a72d4bc39bfcaec97ff
Hekk3 <timurh434@gmail.com>
2021-03-15 11:48:03 +0300

    Second fix

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,20 @@ $(function () {
         }
     });
 });
+
+// fixes
+
+document.addEventListener('mousemove', function() {
+  let mfpArrows = document.querySelectorAll('.mfp-arrow');
+  let kursor = document.querySelector('.kursor');
+
+  for (let i = 0; i < mfpArrows.length; i++) {
+    mfpArrows[i].addEventListener('mouseover', function() {
+      kursor.classList.add('hovering');
+    })
+
+    mfpArrows[i].addEventListener('mouseout', function() {
+      kursor.classList.remove('hovering');
+    })
+  }
+});
