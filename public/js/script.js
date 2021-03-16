68265cced450c58f8ac87ed5fceca883ce8edbb3
Hekk3 <timurh434@gmail.com>
2021-03-16 08:39:45 +0300

    Fix eight

--- public/js/script.js
+++ public/js/script.js
@@ -13,6 +13,24 @@ $(function () {
 document.addEventListener('mousemove', function() {
   let mfpArrows = document.querySelectorAll('.mfp-arrow');
   let kursor = document.querySelector('.kursor');
+  let mfpDown = document.querySelector('.modal-slider .slick-next');
+  let mfpDownSecond = document.querySelector('.modal-slider .slick-prev');
+
+  mfpDown.addEventListener('mouseover', function() {
+    kursor.classList.add('hovering');
+  })
+
+  mfpDown.addEventListener('mouseout', function() {
+    kursor.classList.remove('hovering');
+  })
+
+  mfpDownSecond.addEventListener('mouseover', function() {
+    kursor.classList.add('hovering');
+  })
+
+  mfpDownSecond.addEventListener('mouseout', function() {
+    kursor.classList.remove('hovering');
+  })
 
   for (let i = 0; i < mfpArrows.length; i++) {
     mfpArrows[i].addEventListener('mouseover', function() {
